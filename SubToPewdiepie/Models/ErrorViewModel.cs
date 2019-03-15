138fb0139528e1eff9af1f0449170311172c6508
Paul Gearo <PaulGearo@gmail.com>
2019-03-15 10:27:54 -0500

    Add project files.

---
+++ SubToPewdiepie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SubToPewdiepie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

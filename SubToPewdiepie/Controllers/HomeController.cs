d5e31c00d8769f952d0fee5760c3bab2f3ceb736
Paul Gearo <PaulGearo@gmail.com>
2019-03-21 09:00:28 -0500

    added gif

--- SubToPewdiepie/Controllers/HomeController.cs
+++ SubToPewdiepie/Controllers/HomeController.cs
@@ -10,27 +10,8 @@ namespace SubToPewdiepie.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult MainPage()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
--- SubToPewdiepie/Startup.cs
+++ SubToPewdiepie/Startup.cs
@@ -56,7 +56,7 @@ namespace SubToPewdiepie
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=MainPage}/{id?}");
             });
         }
     }
